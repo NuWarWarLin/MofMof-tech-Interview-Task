9738aa2594a88b35ffd57484e2dbab72ac9cc97c
NuWarWarLin <nuwarwarlin.kakehashi2@gmail.com>
2020-09-04 08:10:42 +0630

    add validation fieldto remark

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,6 +3,7 @@ class Property < ApplicationRecord
   validates :fees, presence: true
   validates :address, presence: true
   validates :year, presence: true
+  validates :remark, presence: true
   has_many :stations, dependent: :destroy
   accepts_nested_attributes_for :stations, allow_destroy: true
 end
